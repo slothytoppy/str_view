8a375c1c4426950b4cd99b8d68f4ed5d582e0c02
slothytoppy <jacobehcalvining@gmail.com>
2024-04-12 13:26:58 -0500

    first version of my sv

---
+++ test.c
@@ -0,0 +1,11 @@
+#include "str_view.h"
+
+int main(void) {
+  str_view sv = {0};
+  str_view dest = sv_from_parts("hel", 3);
+  str_view src = sv_from_parts("lo", 2);
+  sv = sv_concat(dest, src);
+  sv_print(sv);
+  sv = sv_concat(dest, src);
+  return 0;
+}
